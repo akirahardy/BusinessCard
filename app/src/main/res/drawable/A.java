3de46f64d0499843a687d126282ba9cf207f9214
Akira Hardy <akirabhardy@gmail.com>
2024-10-19 13:39:54 -0400

    Initial commit

---
+++ app/src/main/res/drawable/A.java
@@ -0,0 +1,11 @@
+Must include:
+
+The city where the property is marketed is listed
+Firm name registered with the state and real estate brokerage license/ or the regulatory jurisdiction (s) where the firm holds a real estate brokerage license
+The home page must include the firm name as registered, the city and state where the firm is located, and regulatory jurisdictions 
+Must update listings at least once every 31 days to ensure property details are current and not misleading to users
+If any property listings belong to another licensed broker, they must obtain permission, and no changes can be made without the consent of the original listing broker 
+
+
+https://www.nar.realtor/legal/risk-management/nar-internet-advertising-policy#
+
